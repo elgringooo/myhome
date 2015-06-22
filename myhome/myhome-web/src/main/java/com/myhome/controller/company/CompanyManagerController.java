dc8f9baeb983969e3d7fbf15176532853136ee19
Gerald LEVI <Gerald.LEVI3@monext.net>
2015-06-22 17:24:30 +0200

    fix name

--- myhome/myhome-web/src/main/java/com/myhome/controller/company/CompanyManagerController.java
+++ myhome/myhome-web/src/main/java/com/myhome/controller/company/CompanyManagerController.java
@@ -24,85 +24,78 @@ import com.myhome.service.ICompanyService;
 // met en session l attribut asimplecompagny apres l'appel de requestmapping
 public class CompanyManagerController {
 
-	@Autowired
-	private ICompanyService companyService;
+    @Autowired
+    private ICompanyService companyService;
 
-	// ---------- Spring MVC appelle toutes les méthodes @ModelAttribute avant
-	// d’appeler la méthode @RequestMapping -
-	@ModelAttribute("asimplecompagny")
-	public Company populatePersonne(@RequestParam(required = false) String name) {
-		return new Company(name, 50, "Genova");
-	}
+    // ---------- Spring MVC appelle toutes les méthodes @ModelAttribute avant
+    // d’appeler la méthode @RequestMapping -
+    @ModelAttribute("asimplecompagny")
+    public Company populatePersonne(@RequestParam(required = false) String name) {
+        return new Company(name, 50, "Genova");
+    }
 
-	@RequestMapping(value = "/manageCompany", method = RequestMethod.GET)
-	public String display(ModelMap pModel, HttpSession session) {
+    @RequestMapping(value = "/manageCompany", method = RequestMethod.GET)
+    public String display(ModelMap pModel, HttpSession session) {
 
-		System.out.println(pModel);
+        System.out.println(pModel);
 
-		System.out.println(String.format("session = %s ===>",
-				session.getAttribute("asimplecompagny")));
-		final List<Company> companyList = companyService.findAllCompanies();
-		pModel.addAttribute("companyList", companyList);
-		return "company/list";
-	}
+        System.out.println(String.format("session = %s ===>", session.getAttribute("asimplecompagny")));
+        final List<Company> companyList = companyService.findAllCompanies();
+        pModel.addAttribute("companyList", companyList);
+        return "company/list";
+    }
 
-	@RequestMapping(value = "/redirectToCreateCompany", method = RequestMethod.GET)
-	public String redirectToCreate(
-			@ModelAttribute("companyForm") CompanyForm companyForm,
-			ModelMap pModel) {
-		return "company/edit";
-	}
+    @RequestMapping(value = "/redirectToCreateCompany", method = RequestMethod.GET)
+    public String redirectToCreate(@ModelAttribute("companyForm") CompanyForm companyForm, ModelMap pModel) {
+        return "company/edit";
+    }
 
-	@RequestMapping(value = "/redirectToUpdateCompany", method = RequestMethod.GET)
-	public String redirectToUpdate(ModelMap pModel,
-			@ModelAttribute("companyForm") CompanyForm form,
-			@RequestParam(value = "idCompany") final Integer idCompany) {
+    @RequestMapping(value = "/redirectToUpdateCompany", method = RequestMethod.GET)
+    public String
+        redirectToUpdate(ModelMap pModel, @ModelAttribute("companyForm") CompanyForm form, @RequestParam(value = "idCompany") final Integer idCompany) {
+        System.out.println("test DCMVM !!!!!!!!!");
+        Company company = companyService.findCompanyById(idCompany);
+        if (company != null) {
+            form.setId(company.getId());
+            form.setName(company.getName());
+            form.setHeadoffice(company.getHeadoffice());
+            form.setEmployees(String.valueOf(company.getEmployees()));
+        }
 
-		Company company = companyService.findCompanyById(idCompany);
-		if (company != null) {
-			form.setId(company.getId());
-			form.setName(company.getName());
-			form.setHeadoffice(company.getHeadoffice());
-			form.setEmployees(String.valueOf(company.getEmployees()));
-		}
-		return "company/edit";
-	}
+        return "company/edit";
+    }
 
-	@RequestMapping(value = "/saveCompany", params = "save", method = RequestMethod.POST)
-	public String save(
-			@Valid @ModelAttribute(value = "companyForm") final CompanyForm formBean,
-			final BindingResult pBindingResult, final ModelMap pModel) {
+    @RequestMapping(value = "/saveCompany", params = "save", method = RequestMethod.POST)
+    public String save(@Valid @ModelAttribute(value = "companyForm") final CompanyForm formBean, final BindingResult pBindingResult, final ModelMap pModel) {
 
-		if (!pBindingResult.hasErrors()) {
-			Company company = new Company();
-			Long id = formBean.getId();
-			if (id != null) {
-				company.setId(id);
-			}
+        if (!pBindingResult.hasErrors()) {
+            Company company = new Company();
+            Long id = formBean.getId();
+            if (id != null) {
+                company.setId(id);
+            }
 
-			company.setName(formBean.getName());
-			company.setHeadoffice(formBean.getHeadoffice());
-			company.setEmployees(Long.valueOf(formBean.getEmployees()));
-			companyService.saveCompany(company);
-			return "redirect:manageCompany";
-		}
+            company.setName(formBean.getName());
+            company.setHeadoffice(formBean.getHeadoffice());
+            company.setEmployees(Long.valueOf(formBean.getEmployees()));
+            companyService.saveCompany(company);
+            return "redirect:manageCompany";
+        }
 
-		return "company/edit";
-	}
+        return "company/edit";
+    }
 
-	@RequestMapping(value = "/saveCompany", params = "cancel", method = RequestMethod.POST)
-	public String cancelSave(final ModelMap pModel) {
-		return "redirect:manageCompany";
-	}
+    @RequestMapping(value = "/saveCompany", params = "cancel", method = RequestMethod.POST)
+    public String cancelSave(final ModelMap pModel) {
+        return "redirect:manageCompany";
+    }
 
-	@RequestMapping(value = "/deleteCompany", method = RequestMethod.GET)
-	public String delete(
-			@RequestParam(value = "idCompany") final Integer idCompany,
-			final ModelMap pModel) {
-		final Company company = new Company();
-		company.setId(idCompany);
-		companyService.deleteCompany(company);
-		return "redirect:manageCompany";
-	}
+    @RequestMapping(value = "/deleteCompany", method = RequestMethod.GET)
+    public String delete(@RequestParam(value = "idCompany") final Integer idCompany, final ModelMap pModel) {
+        final Company company = new Company();
+        company.setId(idCompany);
+        companyService.deleteCompany(company);
+        return "redirect:manageCompany";
+    }
 
 }
