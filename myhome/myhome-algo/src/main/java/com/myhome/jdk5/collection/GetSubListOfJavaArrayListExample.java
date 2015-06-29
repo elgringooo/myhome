60651589e167752a1b9502eeaf606de5a028be26
Gerald LEVI <Gerald.LEVI3@monext.net>
2015-06-29 14:00:19 +0200

    ok

--- myhome/myhome-algo/src/main/java/com/myhome/jdk5/collection/GetSubListOfJavaArrayListExample.java
+++ myhome/myhome-algo/src/main/java/com/myhome/jdk5/collection/GetSubListOfJavaArrayListExample.java
@@ -1,47 +1,45 @@
 package com.myhome.jdk5.collection;
 
 /*
-  Get Sub List of Java ArrayList Example
-  This Java Example shows how to get sub list of java ArrayList using subList
-  method by providing start and end index.
+ * Get Sub List of Java ArrayList Example This Java Example shows how to get sub list of java ArrayList using subList method by providing start and end index.
  */
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class GetSubListOfJavaArrayListExample {
+/**
+ * @author glevi
+ */
+public final class GetSubListOfJavaArrayListExample {
 
-	public static void main(String[] args) {
+    private GetSubListOfJavaArrayListExample() {
 
-		// create an ArrayList object
-		ArrayList arrayList = new ArrayList();
+    }
 
-		// Add elements to Arraylist
-		arrayList.add("0");
-		arrayList.add("1");
-		arrayList.add("2");
-		arrayList.add("3");
-		arrayList.add("4");
+    public static void main(final String[] args) {
 
-		/*
-		 * To get a sub list of Java ArrayList use List subList(int startIndex,
-		 * int endIndex) method. This method returns an object of type List
-		 * containing elements from startIndex to endIndex - 1.
-		 */
+        // create an ArrayList object
+        final List<String> arrayList = new ArrayList<String>();
 
-		List lst = arrayList.subList(0, 7);
-		
-		
-		
-		
+        // Add elements to Arraylist
+        arrayList.add("0");
+        arrayList.add("1");
+        arrayList.add("2");
+        arrayList.add("3");
+        arrayList.add("4");
 
-		// display elements of sub list.
-		System.out.println("Sub list contains : "+lst);
-	 
+        /*
+         * To get a sub list of Java ArrayList use List subList(int startIndex, int endIndex) method. This method returns an object of type List containing
+         * elements from startIndex to endIndex - 1.
+         */
 
-	}
+        final List lst = arrayList.subList(0, 7);
+
+        // display elements of sub list.
+        System.out.println("Sub list contains : " + lst);
+
+    }
 }
 /*
- * Output would be Sub list contains : 2 3 2 is removed from sub list After
- * removing 2 from sub list original ArrayList contains : 1 3 4 5
+ * Output would be Sub list contains : 2 3 2 is removed from sub list After removing 2 from sub list original ArrayList contains : 1 3 4 5
  */
--- myhome/myhome-algo/src/main/java/com/myhome/jdk8/LambaExpression.java
+++ myhome/myhome-algo/src/main/java/com/myhome/jdk8/LambaExpression.java
@@ -3,11 +3,19 @@ package com.myhome.jdk8;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class Lamba Expression
+ * @author glevi
+ */
 public class LambaExpression {
-	public static void main(String[] args) {
-		List<String> tes = new ArrayList<String>();
-		tes.add("levi");
-		tes.add("gege");
-		tes.stream().forEach(s -> System.out.println(s));
-	}
+    /**
+     * Main
+     * @param args
+     */
+    public static void main(String[] args) {
+        List<String> tes = new ArrayList<String>();
+        tes.add("levi");
+        tes.add("gege");
+        tes.stream().forEach(s -> System.out.println(s));
+    }
 }
